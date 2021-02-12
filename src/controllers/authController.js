abed71871820075f307cb4403a8387ad76937abe
lucas valadares <lucasvvpaiva@hotmail.com>
2021-02-12 16:33:38 -0300

    criação de usuario completo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,15 +5,22 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
     try {
+        if(await User.findOne({ email }))
+            return res.status(400).send({error: 'Usuario ja existe'});
+            
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({ user });
     } catch (err) {
 
-        return res.status(400).send({ error: 'Flaha no Registro '})
+        return res.status(400).send({ error: 'Flaha no Registro' });
     
     }
 });
-
-module.exports = app => ('/auth', router);
+ 
+module.exports = app => app.use('/auth', router);
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+// mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+
 mongoose.Promise = global.Promise;
 
+mongoose.connect("mongodb://localhost/noderest", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+ 
 module.exports = mongoose;
